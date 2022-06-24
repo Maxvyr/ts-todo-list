4a594038e82904df9a620f787f2789848807dc74
maxvyr <maxvid30@gmail.com>
2022-06-24 21:24:44 +0200

    ✨ sava inside local storage

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,14 @@ type Task = {
   createdAt: Date
 }
 
+const localStorageTasks = "TASKS"
 const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks : Task[] = loadTasks()
+
+//start page load all task inside localStorage
+tasks.forEach(addListItem)
 
 
 form?.addEventListener("submit", e => {
@@ -24,7 +29,11 @@ form?.addEventListener("submit", e => {
     createdAt : new Date()
   }
 
+  tasks.push(task)
+  saveTasks()
+
   addListItem(task)
+  input.value = "" //remove text inside form
 })
 
 
@@ -32,9 +41,24 @@ function addListItem(task : Task) : boolean {
   const item = document.createElement("li")
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
   checkbox.type = "checkbox"
+  checkbox.checked = task.completed
   label.append(checkbox, task.title)
   item.append(label)
   list?.append(item)
   return true
+}
+
+function saveTasks() {
+  localStorage.setItem(localStorageTasks, JSON.stringify(tasks))
+}
+
+function loadTasks() : Task[] {
+  const taskJSON = localStorage.getItem(localStorageTasks)
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
 }
